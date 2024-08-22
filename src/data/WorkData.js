8c2463fc5b68371b8473bb27b460456e277e0255
rahulprojects01 <srahulb6@gmail.com>
2024-08-22 13:07:31 +0530

    update

---
+++ src/data/WorkData.js
@@ -0,0 +1,35 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Banking Management System",
+        description:"Implemented OOP principles for user-friendly account, Used core java functionality",
+        tags:["java","oop","collections"],
+        
+        demo:"https://github.com/RahulProjects01/coreJava-Projects/blob/master/BankingManagementSystem.java",
+        github:"https://github.com/RahulProjects01/coreJava-Projects/blob/master/BankingManagementSystem.java"
+    },
+    {
+        id:2,
+        name:"Agrisure",
+        description:"Developed and deployed a blockchain-based solution to provide farmers with a reliable and transparent insurance  platform",
+        tags:["react","next", "blockchain", "js"],
+        
+        demo:"https://chainlink-agrisure.vercel.app/",
+        github:"https://github.com/vikasjangid1/chainlink-agrisure"
+        
+    },
+    {
+        id:3,
+        name:"Cafe Management System",
+        description:"Displayed the Menu, Order, display, and Bill, Implemented OOP principles for user-friendly account.",
+        tags:["java","oop","collections"],
+        
+        demo:"http://devto-clone-codebucks27.vercel.app/",
+        github:"https://github.com/codebucks27/devto-clone"
+    },
+   
+    
+  
+] 
